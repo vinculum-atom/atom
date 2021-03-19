1cf9e128fcc67c854cff7965ee52869cb1a2bc85
Steve Breker <sbreker@artefactual.com>
2021-03-19 23:49:14 +0000

    Added parentId column check
    - Added ability to inject options into each test
    - Added mock QubitFlatfileImport
    - Added --source option
    - Added --class-name option (default QubitInformationObject)

--- lib/task/import/csvCheckImportTask.class.php
+++ lib/task/import/csvCheckImportTask.class.php
@@ -48,6 +48,15 @@ class csvCheckImportTask extends arBaseTask
         sfCommandOption::PARAMETER_NONE,
         'Provide detailed information regarding each test'
       ),
+      new sfCommandOption('source', null,
+        sfCommandOption::PARAMETER_REQUIRED,
+        'Source name for validating parentId matching against previous imports. If not set, parentId validation against AtoM\'s database will be skipped.'
+      ),
+      new sfCommandOption('class-name', null,
+        sfCommandOption::PARAMETER_REQUIRED,
+        'Qubit object type contained in CSV.',
+        'QubitInformationObject'
+      ),
     ));
 
     $this->namespace = 'csv';
@@ -115,6 +124,8 @@ EOF;
     //$validatorOptions = [];
     $keymap = [
       'verbose'           => 'verbose',
+      'source'            => 'source',
+      'class-name'        => 'className',
     ];
 
     foreach ($keymap as $oldkey => $newkey)
@@ -133,6 +144,8 @@ EOF;
   protected function validateOptions($options = [])
   {
     // Throw exception here if set option is invalid.
+    
+    // TODO: Add validation of class-name
   }
 
   protected function printResults(array $results)
@@ -153,11 +166,11 @@ EOF;
           printf("%s\n", $line);
         }
 
-        if ($this->verbose && 0 < count($testResult['affectedRows']))
+        if ($this->verbose && 0 < count($testResult['details']))
         {
           printf("\nDetails:\n");
 
-          foreach($testResult['affectedRows'] as $line)
+          foreach($testResult['details'] as $line)
           {
             printf("%s\n", $line);
           }
