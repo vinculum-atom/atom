1d54f2671883f1be2125ce7a23fd603502900e6f
Steve Breker <sbreker@artefactual.com>
2021-05-11 22:57:40 +0000

    CSV Checker WebUI integration, refs #99999
    - Added Settings page for config of setting
    - Added Migration to add setting fixture
    - Added fixture to settings.yml

---
+++ apps/qubit/modules/settings/templates/csvValidatorSuccess.php
@@ -0,0 +1,46 @@
+<?php decorate_with('layout_2col.php'); ?>
+
+<?php slot('sidebar'); ?>
+
+  <?php echo get_component('settings', 'menu'); ?>
+
+<?php end_slot(); ?>
+
+<?php slot('title'); ?>
+
+  <h1><?php echo __('CSV Validator'); ?></h1>
+
+<?php end_slot(); ?>
+
+<?php slot('content'); ?>
+
+  <?php echo $form->renderGlobalErrors(); ?>
+
+  <?php echo $form->renderFormTag(url_for(['module' => 'settings', 'action' => 'csvValidator'])); ?>
+
+    <?php echo $form->renderHiddenFields(); ?>
+
+    <div id="content">
+
+      <fieldset class="collapsible">
+
+        <legend><?php echo __('CSV Validator settings'); ?></legend>
+
+        <?php echo $form->csv_validator_default_import_behaviour
+            ->label(__('CSV Validator default behaviour when CSV Import is run'))
+            ->help(__('Configure the CSV Validator to validate CSV contents prior to import'))
+            ->renderRow(); ?>
+
+      </fieldset>
+
+    </div>
+
+    <section class="actions">
+      <ul>
+        <li><input class="c-btn c-btn-submit" type="submit" value="<?php echo __('Save'); ?>"/></li>
+      </ul>
+    </section>
+
+  </form>
+
+<?php end_slot(); ?>
---
+++ lib/task/migrate/migrations/arMigration0191.class.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Modify database for caption/subtitle/chapter support.
+ *
+ * @package    AccesstoMemory
+ * @subpackage migration
+ */
+class arMigration0191
+{
+    const VERSION = 191;
+    const MIN_MILESTONE = 2;
+
+    public function up($configuration)
+    {
+        // Add CSV Validator settings.
+        if (null === QubitSetting::getByName('csv_validator_default_import_behaviour')) {
+            $setting = new QubitSetting();
+            $setting->name = 'csv_validator_default_import_behaviour';
+            $setting->value = 0;
+            $setting->editable = 1;
+            $setting->source_culture = 'en';
+            $setting->save();
+        }
+
+        return true;
+    }
+}
